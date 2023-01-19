e824ca9100fbf3b370a11bb265f460990a8fb426
tnrla75 <73986336+tnrla75@users.noreply.github.com>
2023-01-20 01:02:38 +0900

    Add files via upload

---
+++ 162-createDB.php
@@ -0,0 +1,23 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $pw = "";
+
+    $dbConnect = new mysqli($host, $user, $pw);
+
+    $dbConnect -> set_charset("utf8");
+
+
+    if(mysqli_connect_errno()){
+        echo "DB 접속 실패";
+    }else {
+        $sql = "CREATE DATABASE php200project";
+        $res = $dbConnect->query($sql);
+        echo "DB 접속 성공";
+        if($res){
+            echo "DB 생성 완료";
+        }else{
+            echo "DB 생성 실패";
+        }
+    }
+?>
---
+++ 170-index.php
@@ -0,0 +1,32 @@
+<?php
+    include $_SERVER['DOCUMENT_ROOT'].'/phpstudy/171-session.php';
+//echo $_SERVER['DOCUMENT_ROOT'].'/phpstudy/171-session.php';
+?>
+
+<html>
+<head>
+
+</head>
+<body>
+<?php
+    if(!isset($_SESSION['memberID'])){
+?>
+        <a href="/173-signUpForm.php">회원가입</a>
+        <br>
+        <a href="/175-signInForm.php">로그인</a>
+<?php
+    }else{
+?>
+        <a href="/183-list.php">게시판</a>
+        <br>
+        <a href="/189-surveyForm.php">설문조사 프로그램</a>
+        <br>
+        <a href="/195-1-surveyResultBarChart.php">투표결과 바차트로 보기</a>
+        <br>
+        <a href="/195-2-surveyResultPiechart.php">투표결과 파이차트로 보기</a>
+<?php
+    }
+?>
+</body>
+</html>
+
