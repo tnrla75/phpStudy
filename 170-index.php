2ac9a41547b51dfc02c5b87f4e41fa42d8ac326c
tnrla75 <tnrla75@naver.com>
2023-01-24 22:01:29 +0900

    163 - connection 파일 작업

--- 170-index.php
+++ 170-index.php
@@ -1,6 +1,7 @@
 <?php
     include $_SERVER['DOCUMENT_ROOT'].'/php_study/171-session.php';
 //echo $_SERVER['DOCUMENT_ROOT'].'/phpstudy/171-session.php';
+
 ?>
 
 <html>
@@ -11,9 +12,9 @@
 <?php
     if(!isset($_SESSION['memberID'])){
 ?>
-        <a href="/173-signUpForm.php">회원가입</a>
+        <a href="173-signUpform.php">회원가입</a>
         <br>
-        <a href="/175-signInForm.php">로그인</a>
+        <a href="175-signInForm.php">로그인</a>
 <?php
     }else{
 ?>
@@ -25,7 +26,11 @@
         <br>
         <a href="/195-2-surveyResultPiechart.php">투표결과 파이차트로 보기</a>
         <br>
-        <a href="/webEditor/196-editorForm.php">간단한 코딩 에디터</a>
+        <a href="/196-editorForm.php">간단한 코딩 에디터</a>
+        <br>
+        <a href="/200-1-selectForm.php">실시간 검색어 1위 키워드 보기</a>
+        <br>
+        <a href="177-signOut.php">로그아웃</a>
 <?php
     }
 ?>
